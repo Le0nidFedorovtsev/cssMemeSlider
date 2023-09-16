09c759a2e749c5a0d531dbcd16d1270ebde8dc1d
Leonid <leo.fedorovtsev@gmail.com>
2023-09-16 18:02:12 +0500

    feat: add text chenging and button animation, fix some errors Sat, Sep 16, 2023 06:02:12 PM

--- cssMemeSlider/index.js
+++ cssMemeSlider/index.js
@@ -2,7 +2,11 @@
 
 document.addEventListener('click', (e) => {
     const sliderLine = document.getElementById('sliderLine')
-    const memText = document.getElementById('memText')
+
+    const memText1 = document.getElementById('memText1')
+    const memText2 = document.getElementById('memText2')
+    const memText3 = document.getElementById('memText3')
+    const memText4 = document.getElementById('memText4')
 
     const memButtonOne = document.getElementById('memButtonOne')
     const memButtonTwo = document.getElementById('memButtonTwo')
@@ -20,12 +24,54 @@ document.addEventListener('click', (e) => {
     const click_at_memButtonWrapFour = e.composedPath().includes(memButtonWrapFour);
     if (click_at_memButtonWrapOne) {
         sliderLine.style.marginLeft = "0"
-        
+        memText1.classList.add('active');
+        memText1.classList.remove('none');
+        memText2.classList.remove('active');
+        memText3.classList.remove('active');
+        memText4.classList.remove('active');
+
+        memButtonOne.classList.add("active")
+        memButtonTwo.classList.remove("active")
+        memButtonThree.classList.remove("active")
+        memButtonFour.classList.remove("active")
     } else if (click_at_memButtonWrapTwo) {
         sliderLine.style.marginLeft = "-100%"
+        memText1.classList.remove('active');
+        memText2.classList.add('active');
+        memText3.classList.remove('active');
+        memText4.classList.remove('active');
+
+        memText1.classList.add('none');
+
+        memButtonOne.classList.remove("active")
+        memButtonTwo.classList.add("active")
+        memButtonThree.classList.remove("active")
+        memButtonFour.classList.remove("active")
     } else if (click_at_memButtonWrapThree) {
         sliderLine.style.marginLeft = "-200%"
+        memText1.classList.remove('active');
+        memText2.classList.remove('active');
+        memText3.classList.add('active');
+        memText4.classList.remove('active');
+
+        memText1.classList.add('none');
+
+        memButtonOne.classList.remove("active")
+        memButtonTwo.classList.remove("active")
+        memButtonThree.classList.add("active")
+        memButtonFour.classList.remove("active")
     } else if (click_at_memButtonWrapFour) {
         sliderLine.style.marginLeft = "-300%"
+        memText1.classList.remove('active');
+        memText2.classList.remove('active');
+        memText3.classList.remove('active');
+        memText4.classList.add('active');
+
+        memText1.classList.add('none');
+
+        memButtonOne.classList.remove("active")
+        memButtonTwo.classList.remove("active")
+        memButtonThree.classList.remove("active")
+        memButtonFour.classList.add("active")
     }
 });
